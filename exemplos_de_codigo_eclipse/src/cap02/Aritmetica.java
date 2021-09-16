227203651856b839caf6972f2bef846a110c4899
WINglyst4rk <wingly205@gmail.com>
2021-09-16 18:26:55 -0300

    finalização da classe de aritméticos

--- exemplos_de_codigo_eclipse/src/cap02/Aritmetica.java
+++ exemplos_de_codigo_eclipse/src/cap02/Aritmetica.java
@@ -7,16 +7,16 @@ public class Aritmetica {
 		int a = 5, b = 2;
 		
 		System.out.println("Valores: a = " + a + " b = " + b);
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		System.out.println();
-		System.out.println();
+		System.out.println(" -b = " + (-b));
+		System.out.println("a + b = " + (a + b));
+		System.out.println("a - b = " + (a - b));
+		System.out.println("a * b = " + (a * b));
+		System.out.println("a / b = " + (a / b));
+		System.out.println("(float) a / b = " + ((float)a / b));
+		System.out.println("a % b = " + (a % b));
+		System.out.println("a++ = " + a++);
+		System.out.println(" b = " + ( b));
+		System.out.println("Valores: a = " + a + ", b =" + b);
 	}
 
 }
