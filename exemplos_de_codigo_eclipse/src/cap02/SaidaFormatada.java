f6568890c02860a483e2899dd6871e3698d992a6
WINglyst4rk <wingly205@gmail.com>
2021-09-16 17:28:48 -0300

    exercicio sobre saida formatada

---
+++ exemplos_de_codigo_eclipse/src/cap02/SaidaFormatada.java
@@ -0,0 +1,29 @@
+package cap02;
+
+import java.util.Scanner;
+
+public class SaidaFormatada {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("No final ? ");
+
+		int limite = sc.nextInt();
+
+		int soma = 0;
+
+		for (int i = 0; i <= limite; i++) {
+
+			System.out.printf("%3da. Soma parcial = %6d%n", i, soma);
+			soma += i;
+
+		}
+
+		System.out.printf("Soma total[0..%3d] = %6d%n", limite, soma);
+
+		sc.close();
+	}
+
+}
