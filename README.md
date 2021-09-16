be17f23fbd5c2b442887874f7bbd1770a5abb08f
WINglyst4rk <wingly205@gmail.com>
2021-09-16 17:37:23 -0300

    Adição do README.md e classe sobre operadores aritméticos

---
+++ README.md
@@ -0,0 +1,12 @@
+# Java Guia do Programador 4º edição
+
+
+
+## Códigos do livro guia do programador
+
+
+
+
+
+
+
---
+++ exemplos_de_codigo_eclipse/src/cap02/Aritmetica.java
@@ -0,0 +1,22 @@
+package cap02;
+
+public class Aritmetica {
+	
+	public static void main(String[] args) {
+		
+		int a = 5, b = 2;
+		
+		System.out.println("Valores: a = " + a + " b = " + b);
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+	}
+
+}
