1e24db3ef52e9c5918355da41f15b9d73d0a5949
WINglyst4rk <wingly205@gmail.com>
2021-09-26 12:35:42 -0300

    adição de comentarios para explicação

--- exemplos_de_codigo_eclipse/src/cap02/P0227Assert.java
+++ exemplos_de_codigo_eclipse/src/cap02/P0227Assert.java
@@ -6,17 +6,17 @@ public class P0227Assert {
 
 	public static void main(String[] args) {
 
-		Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in); // entrada de dados
 
 		System.out.print("Forneça inteiro > 9: ");
-		int valor = sc.nextInt();
+		int valor = sc.nextInt(); // lê valor inteiro
 
-		assert valor > 9;
-		int soma = 0;
+		assert valor > 9; // esperado valor maior que 9
+		int soma = 0; // declara variável para soma
 
-		for (int i = 1; i < valor; i++) {
+		for (int i = 1; i < valor; i++) { // realiza laço para efetuar soma
 			soma += i;
-			assert soma < 99 : "soma(" + soma + ") >= 99";
+			assert soma < 99 : "soma(" + soma + ") >= 99"; // esperado soma < 99
 		}
 		System.out.println("Soma[1.." + valor + "] = " + soma);
 		sc.close();
