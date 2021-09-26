a79778cb18513f78314333903f856003273ce295
WINglyst4rk <wingly205@gmail.com>
2021-09-26 12:38:13 -0300

    adição de explicações

--- exemplos_de_codigo_eclipse/src/cap02/P0228Arranjo.java
+++ exemplos_de_codigo_eclipse/src/cap02/P0228Arranjo.java
@@ -6,19 +6,19 @@ public class P0228Arranjo {
 
 	public static void main(String[] args) {
 
-		int a[] = new int[10];
+		int a[] = new int[10]; // declaração e alocação do arranjo inteiro
 
-		Scanner sc = new Scanner(System.in);
-		for (int i = 0; i < a.length; i++) {
+		Scanner sc = new Scanner(System.in); // prepara a entrada de dados
+		for (int i = 0; i < a.length; i++) { // lê valores para o arranjo
 			System.out.println("a[" + i + "]? ");
 			a[i] = sc.nextInt();
 		}
-		int soma = 0;
+		int soma = 0; // exibe arranjo e soma seus elementos
 		for (int i = 0; i < a.length; i++) {
 			System.out.println("a[" + i + "] = " + a[i]);
 			soma += a[i];
 		}
-		System.out.println("Soma = " + soma);
+		System.out.println("Soma = " + soma); // exibe a soma dos elementos
 		sc.close();
 	}
 }
