5265e956199d9c0cf88cf15daabf7b6620bd7f6a
WINglyst4rk <wingly205@gmail.com>
2021-10-03 15:34:52 -0300

    adicionando explicações

--- exemplos_de_codigo_eclipse/src/cap02/P0233Eco2.java
+++ exemplos_de_codigo_eclipse/src/cap02/P0233Eco2.java
@@ -6,14 +6,16 @@ public class P0233Eco2 {
 		
 		for(int i = 0; i < args.length; i++) {
 			System.out.printf("Argumento #%d: %s ", i, args[i]);
-			try { 
+			try { // verifica se o argumento de args[i] é inteiro
 				int n = Integer.parseInt(args[i]);
 				System.out.printf("(inteiro = %d)%n", n);
-			} catch (NumberFormatException e1) {
-				try {
+			} catch (NumberFormatException e1) { // se ocorrer exceção não é inteiro
+				
+				try { // verifica se o argumento de args[i] é real
 					double d = Double.parseDouble(args[i]);
 					System.out.printf("(real = %f)%n", d);
-				} catch (NumberFormatException e2) {
+				} catch (NumberFormatException e2) { // se ocorrer exceção não é real
+					
 					System.out.printf("(String = \"%s\")%n", args[i]);
 				}
 			}
