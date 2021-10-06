0f3e7b6660adae15d0527ad4498a715d30a81217
WINglyst4rk <wingly205@gmail.com>
2021-10-06 10:15:10 -0300

    exibindo 2 casas decimais

--- exemplos_de_codigo_eclipse/src/cap02/P0233Eco2.java
+++ exemplos_de_codigo_eclipse/src/cap02/P0233Eco2.java
@@ -3,19 +3,19 @@ package cap02;
 public class P0233Eco2 {
 
 	public static void main(String[] args) {
-		
-		for(int i = 0; i < args.length; i++) {
+
+		for (int i = 0; i < args.length; i++) {
 			System.out.printf("Argumento #%d: %s ", i, args[i]);
 			try { // verifica se o argumento de args[i] é inteiro
 				int n = Integer.parseInt(args[i]);
 				System.out.printf("(inteiro = %d)%n", n);
 			} catch (NumberFormatException e1) { // se ocorrer exceção não é inteiro
-				
+
 				try { // verifica se o argumento de args[i] é real
 					double d = Double.parseDouble(args[i]);
-					System.out.printf("(real = %f)%n", d);
+					System.out.printf("(real = %.2f)%n", d); // modificando para exibir 2 casas decimais
 				} catch (NumberFormatException e2) { // se ocorrer exceção não é real
-					
+
 					System.out.printf("(String = \"%s\")%n", args[i]);
 				}
 			}
