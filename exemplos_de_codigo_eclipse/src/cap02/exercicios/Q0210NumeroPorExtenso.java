ed96df02101769f2940384b94463cd7dc3f52c90
WINglyst4rk <wingly205@gmail.com>
2021-10-08 10:43:49 -0300

    testando a lógica

---
+++ exemplos_de_codigo_eclipse/src/cap02/exercicios/Q0210NumeroPorExtenso.java
@@ -0,0 +1,14 @@
+package cap02.exercicios;
+
+public class Q0210NumeroPorExtenso {
+
+	/*
+	 * Objetivo: Programa capaz de imprimir no console por extenso um número por extenso
+	 * no intervalo de [0 - 49] qualquer valor fora desse parametro deve ser desconsiderado
+	 * e uma mensagem de erro adeguada deve ser exibida
+	 * 
+	 * Entrada: Um número entre [0-49] inteiro
+	 * 
+	 * Saída: o número recebido por extenso no console
+	 */
+}
