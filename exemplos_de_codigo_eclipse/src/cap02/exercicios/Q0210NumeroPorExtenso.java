c1691b7662c8e58e9fd088948ea8d64066f2ad2b
WINglyst4rk <wingly205@gmail.com>
2021-10-11 09:09:44 -0300

    Estudando mais sobre a lógica

--- exemplos_de_codigo_eclipse/src/cap02/exercicios/Q0210NumeroPorExtenso.java
+++ exemplos_de_codigo_eclipse/src/cap02/exercicios/Q0210NumeroPorExtenso.java
@@ -11,4 +11,51 @@ public class Q0210NumeroPorExtenso {
 	 * 
 	 * Saída: o número recebido por extenso no console
 	 */
+	
+	public static void main(String[] args ) {
+		
+		String[] unidade = new String[10];
+		String[] dezena = new String[7];
+		String[] subDezena = new String[10];
+		
+		subDezena[0] = "dez";
+		subDezena[1] = "onze";
+		subDezena[2] = "doze";
+		subDezena[3] = "treze";
+		subDezena[4] = "quartoze";
+		subDezena[5] = "quinze";
+		subDezena[6] = "dezeseis";
+		subDezena[7] = "desesete";
+		subDezena[8] = "dezoito";
+		subDezena[9] = "dezenove";
+		
+		unidade[0] = "zero";
+		unidade[1] = "um";
+		unidade[2] = "dois";
+		unidade[3] = "três";
+		unidade[4] = "quatro";
+		unidade[5] = "cinco";
+		unidade[6] = "seis";
+		unidade[7] = "sete";
+		unidade[8] = "oito";
+		unidade[9] = "nove";
+		
+		dezena[0] = "vinte";
+		dezena[0] = "trinta";
+		dezena[0] = "quarenta";
+		dezena[0] = "cinquenta";
+		dezena[0] = "sesenta";
+		dezena[0] = "setenta";
+		dezena[0] = "oitenta";
+		dezena[0] = "noventa";
+	
+		
+		try {
+			
+			
+		}catch(NumberFormatException e) {
+			
+		}
+		
+	}
 }
